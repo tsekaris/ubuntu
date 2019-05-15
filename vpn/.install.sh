5c8923086b9e762ec4150e0f31b11e8388777415
tsekaris <tsemix@gmail.com>
2019-05-15 13:51:52 +0300

    Update .install.sh

--- vpn/.install.sh
+++ vpn/.install.sh
@@ -4,5 +4,5 @@ sudo apt-get install software-properties-common -y #Χρήση ppa στο λει
 sudo add-apt-repository ppa:wireguard/wireguard
 #sudo apt-get update #Δεν χρειάζεται στο ubuntu 18.04
 sudo apt-get install wireguard -y
-modprobe wireguard #or reboot. Ενεργοποίηση στον linux πυρήνα
+sudo modprobe wireguard #or reboot. Ενεργοποίηση στον linux πυρήνα
 # lsmod | grep wireguard # Για να δούμε αν έχει ενεργοποιηθεί
