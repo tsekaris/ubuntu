ba8df0b7fd9be08014760947ade8d1e7215ec493
tsekaris <tsemix@gmail.com>
2019-05-17 12:24:18 +0300

    Update .install.sh

--- node/.install.sh
+++ node/.install.sh
@@ -5,7 +5,8 @@
 sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install nodejs -y
-sudo npm update -g
+
 # npm install -g npm
 # node-red install που προτιμάει nodejs LTS έκδοση
+sudo npm update -g
 sudo npm install -g --unsafe-perm node-red
