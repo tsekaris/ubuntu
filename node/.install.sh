67e2319f3efe254c0c296b752b42d4c23c04b10c
tsekaris <tsemix@gmail.com>
2019-05-17 12:28:46 +0300

    Update .install.sh

--- node/.install.sh
+++ node/.install.sh
@@ -2,9 +2,23 @@
 
 # Προτιμάμε την τελευταία LTS έκδοση.
 # Από NodeSource.
-sudo apt-get install curl -y
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install nodejs -y
+nodesource(){
+  sudo apt-get install curl -y
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+  sudo apt-get install nodejs -y
+}
+
+# nvm
+nvm(){
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  nvm install 10.15.1
+  nvm use 10.15.1
+  nvm alias default 10.15.1
+  npm install -g npm
+  echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+}
+
+
 
 # npm install -g npm
 # node-red install που προτιμάει nodejs LTS έκδοση
