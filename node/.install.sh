71d9e9a86a56907105a5580a0f6c9ef92ae0865e
tsekaris <tsemix@gmail.com>
2019-05-17 15:44:11 +0300

    Update .install.sh

--- node/.install.sh
+++ node/.install.sh
@@ -11,7 +11,8 @@ nodesource(){
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
   echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
-  npm install npm -g
+  npm install -g npm 
+  npm install -g --unsafe-perm node-red
   echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 }
 
