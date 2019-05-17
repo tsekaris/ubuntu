3cc3cf4fe4bbca03973e7c3eb9ce849cdb8dc278
tsekaris <tsemix@gmail.com>
2019-05-17 15:17:33 +0300

    Update .install.sh

--- node_red/.install.sh
+++ node_red/.install.sh
@@ -2,8 +2,8 @@
 # Απαραίτητο να έχει εγκατασταθεί το node
 # Παλιά γινόταν εγκατάσταση με το node στο ίδιο script και γινόταν σφάλμα με τον φάκελο ~/.config όπoυ γινόταν του root.
 install(){
-  sudo npm install -g npm #update npm
-  sudo npm install -g --unsafe-perm node-red # Χωρίς nvm απαιτείται sudo
+  npm install -g npm #update npm
+  npm install -g --unsafe-perm node-red # Χωρίς nvm απαιτείται sudo
 }
 #install
 
