09774bac9f83d4e7e20762fc2bd7f4edc9085e3d
tsekaris <tsemix@gmail.com>
2019-05-17 15:46:36 +0300

    Update .install.sh

--- node_red/.install.sh
+++ node_red/.install.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 # Απαραίτητο να έχει εγκατασταθεί το node
-
-install(){
-  npm install -g npm #update npm
-  npm install -g --unsafe-perm node-red # Χωρίς nvm απαιτείται sudo
-}
-install
-
-# NOT WORKING με .config πρόβλημα
+npm install -g --unsafe-perm node-red
