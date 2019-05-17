14eb696ac1df70bc44e5a176624363797ea90e58
tsekaris <tsemix@gmail.com>
2019-05-17 11:01:41 +0300

    Update .install_all.sh

--- system/.install_all.sh
+++ system/.install_all.sh
@@ -4,6 +4,8 @@ sudo apt-get upgrade -y
 mkdir ~/0
 for script in $HOME/.0/*/.install.sh
 do
-    read -p "Install $script (yes: empty - no: not empty)?" yn
-    [ -z "$yn" ] && sh "$script"
+    echo
+    echo "Installing: $script"
+    delay 1
+    sh "$script"
 done
