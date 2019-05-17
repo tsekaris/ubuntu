c0d30ae116d3288de542dd803111233d9b39dbeb
tsekaris <tsemix@gmail.com>
2019-05-17 15:12:33 +0300

    Update .install.sh

--- node/.install.sh
+++ node/.install.sh
@@ -10,6 +10,7 @@ nodesource(){
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
   echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
+  npm install npm -g
   echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 }
 
