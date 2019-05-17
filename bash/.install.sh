f992eab98babe87688b80cf440e5e8c59a8e3da2
tsekaris <tsemix@gmail.com>
2019-05-17 16:12:07 +0300

    Update .install.sh

--- bash/.install.sh
+++ bash/.install.sh
@@ -2,5 +2,5 @@
 
 # Append στο bashrc που ήδη το ubuntu έχει γράψει σε αυτό
 bashrc="~/.0/bash/.dotfiles/.bashrc"
-line="[ -f $bashrc ] && source $bashrc" 
+line="[ -f $bashrc ] && source $bashrc #tsekaris" 
 grep -Fxq "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
