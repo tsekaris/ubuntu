ac01e0aaf7a7419caed3881b4767b4c839087e38
tsekaris <tsemix@gmail.com>
2019-05-17 12:41:26 +0300

    Update .install.sh

--- node/.install.sh
+++ node/.install.sh
@@ -11,7 +11,9 @@ nodesource(){
 
 # nvm
 nvm(){
+  sudo apt-get install curl -y
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  source ~/.bashrc #Δεν λειτουργεί.
   nvm install 10.15.1
   nvm use 10.15.1
   nvm alias default 10.15.1
@@ -19,9 +21,11 @@ nvm(){
   echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 }
 
-
-
-# npm install -g npm
 # node-red install που προτιμάει nodejs LTS έκδοση
-sudo npm update -g
-sudo npm install -g --unsafe-perm node-red
+red(){
+  npm install -g npm #update npm
+  npm install -g --unsafe-perm node-red # Χωρίς nvm απαιτείται sudo
+}
+
+nvm
+red
