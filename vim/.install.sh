c2268f03dc4fb0074671b0142cf85d689a9c6bf7
tsekaris <tsemix@gmail.com>
2019-05-23 14:25:26 +0300

    Update .install.sh

--- vim/.install.sh
+++ vim/.install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-sudo apt-get install vim -y
+ubuntu(){
+  sudo apt-get install vim -y
+}
+
+ppa(){
+  sudo apt-get install software-properties-common -y #Χρήση ppa στο λειτουργικό
+  sudo add-apt-repository ppa:jonathonf/vim -y
+  sudo apt-get update -y
+  sudo apt-get install vim -y
+}
+
+ppa
 cd $HOME/.0/vim
 stow --target=$HOME .dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
