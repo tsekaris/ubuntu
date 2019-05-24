4e884e16ec4eabd769806a1eae776ffed1df3425
tsekaris <tsemix@gmail.com>
2019-05-24 10:06:25 +0300

    ...

--- mpv/.install.sh
+++ mpv/.install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-#sudo apt-get install mpv -y # Για radio.
+ubuntu(){
+    sudo apt-get install mpv -y
+}
+
+ppa(){
+    sudo apt-get install software-properties-common -y #Χρήση ppa στο λειτουργικό
+    sudo add-apt-repository ppa:mc3man/mpv-tests -y
+    sudo apt-get install mpv
+}
+
+ppa
