4c0c6194212f7bdf1c8d58c712aea4fab00b5f98
tsekaris <tsemix@gmail.com>
2019-05-28 12:32:01 +0300

    Update .install.sh

--- mpv/.install.sh
+++ mpv/.install.sh
@@ -7,7 +7,7 @@ ubuntu(){
 ppa(){
     sudo apt-get install software-properties-common -y #Χρήση ppa στο λειτουργικό
     sudo add-apt-repository ppa:mc3man/mpv-tests -y
-    sudo apt-get install mpv
+    sudo apt-get install mpv -y
 }
 
-ppa
+#ppa
